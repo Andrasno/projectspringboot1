ba2b1c91ac4d4e871fc27064cf38a6068b0948f9
Andre Torres <andrasno@gmail.com>
2019-12-15 22:17:34 -0300

    Project created

---
+++ src/test/java/com/andrasno/projectspring2/Projectspring2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andrasno.projectspring2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Projectspring2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
