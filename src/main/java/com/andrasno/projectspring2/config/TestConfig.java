f4d5b3d2c979074d92e04230fe7a63ae054409db
andrasno <andrasno@gmail.com>
2019-12-23 21:11:58 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/andrasno/projectspring2/config/TestConfig.java
+++ src/main/java/com/andrasno/projectspring2/config/TestConfig.java
@@ -1,25 +1,21 @@
 package com.andrasno.projectspring2.config;
 
-import java.time.Instant;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import com.andrasno.projectspring2.entities.Order;
-import com.andrasno.projectspring2.entities.User;
-import com.andrasno.projectspring2.repositories.OrderRepositories;
-import com.andrasno.projectspring2.repositories.UserRepositories;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 
+	@Override
+	public void run(String... args) throws Exception {
+	
+		
+	}
 
 		
 	}
 	
 	
-}
+
